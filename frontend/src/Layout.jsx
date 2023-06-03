f7e8a2beb3f5d22662b916b20c76d432d7625008
Ehab-24 <ehabs1775@gmail.com>
2023-06-03 23:00:50 +0800

    feat - jobs/notifications

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,40 +1,49 @@
 import { Outlet } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
 import useAuthStore from "./store/authStore";
 import useSocketStore from "./store/socketStore";
 import { useEffect } from "react";
-import {io} from "socket.io-client";
+import { io } from "socket.io-client";
 
 const Layout = () => {
-    const { user } = useAuthStore();
-    const {setSocket, socket} = useSocketStore();
+  const { user } = useAuthStore();
+  const { setSocket, socket } = useSocketStore();
 
-    useEffect(()=>{
-        try {         
-            const socketConnect = io(import.meta.env.VITE_SOCKET_URI);
-            setSocket(socketConnect);
-        } catch (error) {
-            console.log(error);
-        }
-    }, []);
+  useEffect(() => {
+    try {
+      const socketConnect = io(import.meta.env.VITE_SOCKET_URI);
+      setSocket(socketConnect);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
+  useEffect(() => {
+    if (user && socket) {
+      socket.emit("addUser", user.email);
+    }
+  }, [user, socket]);
 
-     useEffect(() => {
-        if (user && socket) {
-            socket.emit("addUser", user.email); 
-        }
-    }, [user, socket]);
+  function sendNoti() {
+    const message = "Notification sent by client";
+    const receiverId = "647b3370feaa9e2205e90055";
+    socket.emit("sendMessage", { message, receiverId });
+  }
 
-    return (
-        <div className="min-h-screen">
+  return (
+    <>
+      <button onClick={sendNoti}>Click me to send</button>
+
+      <div className="min-h-screen">
         <Toaster />
         <Header />
         <div className="px-20 pb-20">
-        <Outlet />
+          <Outlet />
         </div>
-        </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Layout;
---
+++ server/jobs/notification.js
@@ -0,0 +1,58 @@
+import cron from "node-cron";
+import EventModel from "../models/Event.js";
+import NotificationModel from "../models/Notification.js";
+import UserModel from "../models/User.js";
+import path from "path";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.resolve(process.cwd(), "../.env") });
+
+async function sendNotification({ message, recipientId, eventId }) {
+  const _notification = {
+    message,
+    recipientId,
+    eventId,
+  };
+  console.log(notification);
+  return await NotificationModel.create(_notification);
+}
+
+mongoose
+  .connect(
+    "mongodb+srv://spoiledwit:IamMydb123@cluster0.ede2ett.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
+
+async function notification() {
+  console.log("Sending notification");
+  const events = await EventModel.find({ status: "active" });
+
+  const index = Math.floor(Math.random() * events.length);
+
+  for (const event of events) {
+    const users = await UserModel.find({ tags: event.tags[index] });
+    for (const user of users) {
+
+        console.log(user)
+
+      const recipientId = user._id;
+      const subject = "New Event";
+      const message = `<p>A new event has been created that matches your interests. Check it out here: <a href="eventor.com/event/${event._id}">eventor.com/event/${event._id}</a></p>`;
+      await sendNotification({ eventId: event._id, recipientId, message });
+    }
+  }
+}
+
+notification();
+
+// cron.schedule("* * * * *", notification);
